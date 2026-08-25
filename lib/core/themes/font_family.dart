a52aaa4a2f8fda05ba033ab98391b5f94b53af80
Saravanan v <v.saravanan.c@gmail.com>
2026-08-25 20:41:09 +0530

    feat: update font families and improve text styles across the app

--- lib/core/themes/font_family.dart
+++ lib/core/themes/font_family.dart
@@ -1,4 +1,6 @@
 abstract final class FontFamily {
   static const String openSans = 'OpenSans';
   static const String borel = 'Borel';
+  static const String sfPro = 'sfPro';
+  static const String sfProD = 'sfProDisplay';
 }
--- lib/core/themes/text_theme.dart
+++ lib/core/themes/text_theme.dart
@@ -1,56 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:todoapp/core/themes/font_family.dart';
 
 class CustomTextTheme {
   static TextTheme get baseTextTheme => const TextTheme(
     headlineLarge: TextStyle(
       fontSize: 24,
       fontWeight: FontWeight.bold,
-      fontFamily: 'OpenSans',
+      fontFamily: FontFamily.sfProD,
     ),
     headlineMedium: TextStyle(
       fontSize: 22,
       fontWeight: FontWeight.w600,
-      fontFamily: 'OpenSans',
+      fontFamily: FontFamily.sfProD,
     ),
     headlineSmall: TextStyle(
       fontSize: 20,
       fontWeight: FontWeight.w500,
-      fontFamily: 'OpenSans',
+      fontFamily: FontFamily.sfPro,
     ),
     titleLarge: TextStyle(
       fontSize: 18,
       fontWeight: FontWeight.w600,
-      fontFamily: 'OpenSans',
+      fontFamily: FontFamily.sfProD,
     ),
     titleMedium: TextStyle(
       fontSize: 16,
       fontWeight: FontWeight.w500,
-      fontFamily: 'OpenSans',
+      fontFamily: FontFamily.sfPro,
     ),
     bodyLarge: TextStyle(
       fontSize: 16,
       fontWeight: FontWeight.w400,
-      fontFamily: 'OpenSans',
+      fontFamily: FontFamily.sfPro,
     ),
     bodyMedium: TextStyle(
       fontSize: 14,
       fontWeight: FontWeight.w400,
-      fontFamily: 'OpenSans',
+      fontFamily: FontFamily.sfPro,
     ),
     labelLarge: TextStyle(
       fontSize: 13,
       fontWeight: FontWeight.w600,
-      fontFamily: 'OpenSans',
+      fontFamily: FontFamily.sfPro,
     ),
     labelMedium: TextStyle(
       fontSize: 12,
       fontWeight: FontWeight.w500,
-      fontFamily: 'OpenSans',
+      fontFamily: FontFamily.sfPro,
     ),
     labelSmall: TextStyle(
       fontSize: 11,
       fontWeight: FontWeight.w400,
-      fontFamily: 'OpenSans',
+      fontFamily: FontFamily.sfPro,
     ),
   );
 }
